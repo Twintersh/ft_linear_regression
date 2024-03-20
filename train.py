27dcb535291c3dfe652b130bec808c750d94107e
Twintersh <tom.wintersheim@gmail.com>
2024-03-20 11:48:00 +0100

    Starting to try implementing the gradient descent algorithm

---
+++ train.py
@@ -0,0 +1,13 @@
+import sys
+import matplotlib.pyplot as plt
+import pandas
+
+if __name__ == "__main__":
+	try:
+
+	execept SystemExit as e:
+
+# questions :
+# 	- What is m ?
+# 	- How to get and what is LearningRate ?
+# 	- Do I need to use the formulas on the data from the data.csv file ?
