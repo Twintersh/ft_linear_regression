422b8a44c521a9b2811a1603ecffe97dcfae03b0
Twintersh <tom.wintersheim@gmail.com>
2024-11-11 17:41:23 +0100

    Etited presision calcul

--- getPrecision.py
+++ getPrecision.py
@@ -3,14 +3,19 @@ from predict import estimatePrice
 from utils import getThetas, getDataset
 from math import sqrt
 
-def meanSquaredError(x, y, t0, t1):
+def determinationCoef(x, y, t0, t1):
     n = float(len(x))
     pred_y = estimatePrice(x, t0, t1)
-    mse = 1/n * sum((y[i] - pred_y[i])**2 for i in range(len(x)))
-    return mse
+    mean_y = sum(pred_y) / n
+    coef =  1 - sum((y - pred_y)**2) / sum((y - mean_y)**2)
+    return coef
 
 if __name__ == "__main__":
-    x, y = getDataset()
-    t0, t1 = getThetas()
-    mse = meanSquaredError(x, y, t0, t1)
-    print(f"Here is the precision of your model : {mse}\n(according to the meanSquaredError function)")
+    try:
+        x, y = getDataset()
+        t0, t1 = getThetas()
+        coef = determinationCoef(x, y, t0, t1)
+        print(f"Here is the precision of your model : {coef}\n(according to the coefficient of determination)")
+
+    except SystemExit as e:
+        print(e)
